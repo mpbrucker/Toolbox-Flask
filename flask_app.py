5e07d3dc3811633d341ecd1951d52b6a3bdf56b5
Matt Brucker <mpbrucker@gmail.com>
2017-03-21 22:47:09 -0400

    Turning in my toolbox.

--- flask_app.py
+++ flask_app.py
@@ -1,3 +1,26 @@
 """
-Put your Flask app code here.
+A simple flask app.
 """
+
+from flask import Flask, request, render_template
+app = Flask(__name__)
+all_names = ["MattyB"]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user_name = request.form['name']
+        user_age = request.form['age']
+        if user_name is not '' and user_age is not '':
+            return render_template('profile.html', name=user_name, age=user_age)
+        else:
+            return render_template('error.html')
+
+
+if __name__ == '__main__':
+    app.run()
